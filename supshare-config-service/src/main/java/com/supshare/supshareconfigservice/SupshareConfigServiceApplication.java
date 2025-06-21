5a05ebfd4646c7e3f65ec968dc9db694c89ae981
ISSAM LAOUMRI <issamlaoumri@gmail.com>
2025-06-21 15:07:38 +0000

    feat: ready base envirenment for microservices : config, discovery and gateway services

--- supshare-config-service/src/main/java/com/supshare/supshareconfigservice/SupshareConfigServiceApplication.java
+++ supshare-config-service/src/main/java/com/supshare/supshareconfigservice/SupshareConfigServiceApplication.java
@@ -2,8 +2,10 @@ package com.supshare.supshareconfigservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SupshareConfigServiceApplication {
 
     public static void main(String[] args) {
