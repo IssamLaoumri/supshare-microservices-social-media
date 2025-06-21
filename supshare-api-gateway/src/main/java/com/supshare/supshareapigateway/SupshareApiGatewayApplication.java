757be2ef3aee6af18e9553763f9ff71bfc35d226
ISSAM LAOUMRI <issamlaoumri@gmail.com>
2025-06-21 14:00:34 +0000

    feat: setup gateway dependencies

---
+++ supshare-api-gateway/src/main/java/com/supshare/supshareapigateway/SupshareApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.supshare.supshareapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupshareApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupshareApiGatewayApplication.class, args);
+	}
+
+}
