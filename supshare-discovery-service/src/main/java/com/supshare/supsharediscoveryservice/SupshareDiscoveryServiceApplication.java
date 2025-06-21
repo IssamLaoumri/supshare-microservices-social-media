0ea8673b752a30cceef8ae746b837a0167d96e5e
ISSAM LAOUMRI <issamlaoumri@gmail.com>
2025-06-21 14:06:59 +0000

    feat: setup discovery service and update spring web dependecy in the gateway into the reactive web

---
+++ supshare-discovery-service/src/main/java/com/supshare/supsharediscoveryservice/SupshareDiscoveryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.supshare.supsharediscoveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupshareDiscoveryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupshareDiscoveryServiceApplication.class, args);
+    }
+
+}
---
+++ supshare-discovery-service/src/test/java/com/supshare/supsharediscoveryservice/SupshareDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supshare.supsharediscoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupshareDiscoveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
