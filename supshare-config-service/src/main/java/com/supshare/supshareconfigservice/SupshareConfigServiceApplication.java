f5fb7ff15ad6c7079bdbf866285fc8bdc2c135ee
ISSAM LAOUMRI <issamlaoumri@gmail.com>
2025-06-21 14:09:55 +0000

    feat: setup config service base  dependencies

---
+++ supshare-config-service/src/main/java/com/supshare/supshareconfigservice/SupshareConfigServiceApplication.java
@@ -0,0 +1,13 @@
+package com.supshare.supshareconfigservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupshareConfigServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupshareConfigServiceApplication.class, args);
+    }
+
+}
---
+++ supshare-config-service/src/test/java/com/supshare/supshareconfigservice/SupshareConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supshare.supshareconfigservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupshareConfigServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
